2aac170fd75b7e221841374e410d2a97213c0f5c
Zucheng Zhang <zuzhang@mymail.mines.edu>
2016-02-23 16:13:19 -0700

    eyes are now smaller in width

--- GitHub-Exercise-CSCI306/src/Cat.java
+++ GitHub-Exercise-CSCI306/src/Cat.java
@@ -12,7 +12,7 @@ public class Cat {
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
 	// pick eye dimensions
 	private static final int EYE_HEIGHT = 15;
-	private static final int EYE_WIDTH = 50;
+	private static final int EYE_WIDTH = 30;
 	// pick mouth height, width is based on head dimension
 	private static final int MOUTH_HEIGHT = 50;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/2;
